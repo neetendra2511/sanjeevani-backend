9f8ba7ccb7865f05b02990c7dd99bde2ea4be058
neetendra2511 <neetendrasinghknp2019@gmail.com>
2024-07-16 23:24:03 +0530

    vercel deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+    user:"neetendrasinghknp2019@gmail.com",
+    clientId:"82435105580-ghhf8nmbe5e5q3tqkooselilfdprv0qt.apps.googleusercontent.com",
+    clientSecret:"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",
+    refreshToken:"1//04hFrvCMEgXkzCgYIARAAGAQSNwF-L9Ir0gwQLrSJ6YTIojDDzjq8skAQ_5IwZoynTzunOSsTEIfLHFyeyx-v_roUbamnuZNC_Tg"
+}
